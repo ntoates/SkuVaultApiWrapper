6588edc179d9aedcb0bf268d1a85412e19c23819
Nathan Oates <ntoatesky@gmail.com>
2021-11-22 16:43:54 -0500

    format

--- ExampleConsoleApp/Program.cs
+++ ExampleConsoleApp/Program.cs
@@ -7,31 +7,31 @@ using System.Configuration;
 
 namespace ExampleConsoleApp
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-				.AddUserSecrets<SkuVaultApiClientConfig>()
-				.Build();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<SkuVaultApiClientConfig>()
+                .Build();
 
-			// Inject Http client and Config into our ExampleClass
-			IServiceCollection services = new ServiceCollection();
-			services.AddHttpClient<ISkuVaultApiClient, SkuVaultApiClient>(client =>
-			{
-				client.BaseAddress = new Uri("https://testing.com");
-				// Can configure http client here as needed
-			});
-			services.Configure<SkuVaultApiClientConfig>(builder.GetSection("SkuVaultApiClientConfig"));
+            // Inject Http client and Config into our ExampleClass
+            IServiceCollection services = new ServiceCollection();
+            services.AddHttpClient<ISkuVaultApiClient, SkuVaultApiClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://testing.com");
+                // Can configure http client here as needed
+            });
+            services.Configure<SkuVaultApiClientConfig>(builder.GetSection("SkuVaultApiClientConfig"));
 
-			services.AddTransient<IClassThatDoesStuff, ClassThatDoesStuff>();
+            services.AddTransient<IClassThatDoesStuff, ClassThatDoesStuff>();
 
 
 
-			// Get Builder and Run
-			IServiceProvider provider = services.BuildServiceProvider();
+            // Get Builder and Run
+            IServiceProvider provider = services.BuildServiceProvider();
             var exampleClass = provider.GetService<IClassThatDoesStuff>();
             exampleClass?.Run();
-		}
-	}
+        }
+    }
 }
